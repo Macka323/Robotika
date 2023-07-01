844f5c6ae56b6a46b6fe749009e65f72a0624dd8
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2023-07-01 13:05:15 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,19 @@ Servo wrist_rot;
 Servo wrist_ver;
 Servo gripper;
 
+// Joint and id default positions
 int id = 0, m1 = 90, m2 = 90, m3 = 90, m4 = 90, m5 = 90, m6 = 73;
 
 void setup()
 {
-  // put your setup code here, to run once:
+  // Arm Speed
   Serial.begin(115200);
   Braccio.begin();
 }
 
 void loop()
 {
-
+  // Printing every joint's default position
   Serial.print("  id=");
   Serial.print(id);
   Serial.print("  m1=");
@@ -36,15 +37,18 @@ void loop()
   Serial.println(m6);
 
   String command = Serial.readStringUntil('\n');
+
   if (command[0] == 'i' && command[1] == 'd')
   {
-
+    // Multiplies the variables so they fit within the motor's parameters
     id = (command[5] - 48);
     id += (command[4] - 48) * 10;
     id += (command[3] - 48) * 100;
   }
+  
   if (command[0] == 'm')
   {
+    // Switches between each joint
     switch (command[1])
     {
     case '1':
@@ -82,8 +86,8 @@ void loop()
     }
   }
 
+  // Inserting the movement variables
   Braccio.ServoMovement(10, m1, m2, m3, m4, m5, m6);
-
   Serial.print("end=");
   Serial.println(id);
 }
